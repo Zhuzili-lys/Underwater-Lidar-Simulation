9d930a0287a08998a52d65f524bd2cd844d9a1bd
Zhuziliwww <56387848+Zhuzili-lys@users.noreply.github.com>
2022-11-14 19:52:17 +0800

    Add files via upload

--- P_M.m
+++ P_M.m
@@ -1,7 +1,7 @@
 function ret = P_M(t,F_M,TAU, TAU_0)
 %% This function gets the signal received by the model of the backscattering underwater described by the formula:
 % P_m(t) = P_0(t) ** [H_b(t)+H_t(t)]
-% By Zhuziliwww 2022/11/22
+% By Zhuziliwww 2022/11/12
 arguments
     t = 0;
     F_M = 225*(10^6); % modulation frequency in Hz
@@ -12,8 +12,8 @@ end
     ALPHA = 0.2; % Seawater Attenuation coef in m-1
     V = 2.25*(10^(-1)); % speed of light underwater in m*(ns-1)
     N = 10000; % backscatter times
-    RHO_B = 15; % backscatter rate in sea water
-    RHO_T = 0.8; % underwater target reflectivity
+    RHO_B = 0.06; % backscatter rate in sea water
+    RHO_T = 0.2; % underwater target reflectivity
     T_D = 60; % target distance (corresponding time) in ns    
     T_SURFACE = 2; % distance from emitter to surface in ns
     
--- main.m
+++ main.m
@@ -3,15 +3,15 @@ ALPHA = 0.2; % Seawater Attenuation coef in m-1
 V = 2.25*(10^(-1)); % speed of light underwater in m*(ns-1)
 N = 10000; % backscatter times
 RHO_B = 0.06; % backscatter rate in sea water
-RHO_T = 20; % underwater target reflectivity
+RHO_T = 0.2; % underwater target reflectivity
 T_O = 50; % transmiter center delay in ns
 T_D = 150; % target distance (corresponding time) in ns
-F_M = 225*(10^4); % modulation frequency in Hz
+F_M = 225*(10^6); % modulation frequency in Hz
 M = 1; % modulation depth 
 T_SAMP = 0.25; % sampling interval in ns
 TAU_0 = 0; % laser half width in ns
 TAU = 3; % laser impulse width in ns
-P0 = 1000000; % Amplitude of the generated impulse
+P0 = 1; % Amplitude of the generated impulse
 T_SURFACE = 0; % distance from emitter to surface in ns
 
 F_M1 = 225*(10^6);
@@ -20,10 +20,17 @@ t = 1:T_SAMP:400;
 sig1 = zeros(length(t),1);
 sig2 = zeros(length(t),1);
 for ii = 1:length(t)
-    sig1(ii) = P_M(t(ii), F_M1, 15, 57);
-    sig2(ii) = P_M(t(ii), F_M2, 15, 57);
+    sig1(ii) = P_M(t(ii), F_M1, 10, 57);
+    sig2(ii) = P_M(t(ii), F_M2, 10, 57);
 end
+plot(t,sig1); hold on;
+plot(t, sig2);
+hold off;
+sig = [sig1';sig2'];
+whiteSig = sigPrewhiten(sig);
+weight = fastICA(whiteSig,2,0.003);
+
+S = weight'*sig;
+plot(t,S(1,:)); hold on; plot(t, S(2,:));
 % sig1 = sig1/max(sig1);
 % sig2 = sig2/max(sig2);
-plot(t,sig1); hold on;
-plot(t,sig2); 
