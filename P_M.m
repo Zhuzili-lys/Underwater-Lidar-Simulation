14badd204dace9de9d27bfd759cf6e4c783a8285
Zhuziliwww <56387848+Zhuzili-lys@users.noreply.github.com>
2022-11-14 10:52:18 +0800

    Add files via upload

--- P_M.m
+++ P_M.m
@@ -1,33 +1,31 @@
-function ret = P_M(t)
+function ret = P_M(t,F_M,TAU, TAU_0)
 %% This function gets the signal received by the model of the backscattering underwater described by the formula:
 % P_m(t) = P_0(t) ** [H_b(t)+H_t(t)]
 % By Zhuziliwww 2022/11/22
-
+arguments
+    t = 0;
+    F_M = 225*(10^6); % modulation frequency in Hz
+    TAU = 30; % laser impulse width in ns
+    TAU_0 = 0; % laser half width in ns
+end
     %Initialization of the parameters
     ALPHA = 0.2; % Seawater Attenuation coef in m-1
     V = 2.25*(10^(-1)); % speed of light underwater in m*(ns-1)
     N = 10000; % backscatter times
-    RHO_B = 0.06; % backscatter rate in sea water
-    RHO_T = 0.2; % underwater target reflectivity
-    T_O = 50; % transmiter center delay in ns
-    T_D = 150; % target distance (corresponding time) in ns
-    F_M = 225*(10^6); % modulation frequency in Hz
-    M = 1; % modulation depth 
-    T_SAMP = 0.25; % sampling interval in ns
-    TAU_0 = 0; % laser half width in ns
-    TAU = 3; % laser impulse width in ns
-    P0 = 1000000; % Amplitude of the generated impulse
-    T_SURFACE = 10; % distance from emitter to surface in ns
+    RHO_B = 15; % backscatter rate in sea water
+    RHO_T = 0.8; % underwater target reflectivity
+    T_D = 60; % target distance (corresponding time) in ns    
+    T_SURFACE = 2; % distance from emitter to surface in ns
     
     % Impulse Generating
     t_t = t-T_SURFACE;
     t_i = linspace(T_SURFACE,T_SURFACE+T_D, N);
     
-    ret = RHO_T*exp(-2*ALPHA*V*t_t)*P_0(T_SURFACE);
+    ret = RHO_T*exp(-2*ALPHA*V*t_t)*P_0(T_SURFACE,F_M,TAU, TAU_0);
     
     %backscattering
     for bsIdx = 1:N
-        ret = ret + RHO_B*exp(-2*ALPHA*V*t_i(bsIdx))*P_0(t-t_i(bsIdx));
+        ret = ret + RHO_B*exp(-2*ALPHA*V*t_i(bsIdx))*P_0(t-t_i(bsIdx),F_M,TAU, TAU_0);
     end
 
 end
--- main.m
+++ main.m
@@ -3,10 +3,10 @@ ALPHA = 0.2; % Seawater Attenuation coef in m-1
 V = 2.25*(10^(-1)); % speed of light underwater in m*(ns-1)
 N = 10000; % backscatter times
 RHO_B = 0.06; % backscatter rate in sea water
-RHO_T = 0.2; % underwater target reflectivity
+RHO_T = 20; % underwater target reflectivity
 T_O = 50; % transmiter center delay in ns
 T_D = 150; % target distance (corresponding time) in ns
-F_M = 225*(10^6); % modulation frequency in Hz
+F_M = 225*(10^4); % modulation frequency in Hz
 M = 1; % modulation depth 
 T_SAMP = 0.25; % sampling interval in ns
 TAU_0 = 0; % laser half width in ns
@@ -14,9 +14,16 @@ TAU = 3; % laser impulse width in ns
 P0 = 1000000; % Amplitude of the generated impulse
 T_SURFACE = 0; % distance from emitter to surface in ns
 
+F_M1 = 225*(10^6);
+F_M2 = 3*F_M1;
 t = 1:T_SAMP:400;
-sig = zeros(length(t),1);
+sig1 = zeros(length(t),1);
+sig2 = zeros(length(t),1);
 for ii = 1:length(t)
-    sig(ii) = P_M(t(ii));
+    sig1(ii) = P_M(t(ii), F_M1, 15, 57);
+    sig2(ii) = P_M(t(ii), F_M2, 15, 57);
 end
-plot(t,sig);
+% sig1 = sig1/max(sig1);
+% sig2 = sig2/max(sig2);
+plot(t,sig1); hold on;
+plot(t,sig2); 
